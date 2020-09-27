1fe5646f4a788eb48ef721ffaca2c517102b13f2
OmameBeans <62434668+OmameBeans@users.noreply.github.com>
2020-09-27 20:11:14 +0900

    Update RAQ.cpp

--- RAQ.cpp
+++ RAQ.cpp
@@ -7,7 +7,7 @@ struct RAQ {
     const T INF = numeric_limits<T>::max();
     int n;
     vector<T> dat, lazy;
-    RMQ(int n_) : n(), dat(n_ * 4, 0), lazy(n_ * 4, 0) {
+    RAQ(int n_) : n(), dat(n_ * 4, 0), lazy(n_ * 4, 0) {
         int x = 1;
         while (n_ > x) x *= 2;
         n = x;
