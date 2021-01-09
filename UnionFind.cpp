90b24123451ea3cdda6f159307bb2c9c13a87723
OmameBeans <62434668+OmameBeans@users.noreply.github.com>
2021-01-09 23:19:31 +0900

    Update UnionFind.cpp

--- UnionFind.cpp
+++ UnionFind.cpp
@@ -1,9 +1,10 @@
 struct UnionFind {
     vector <ll> par; // 各元の親を表す配列
     vector <ll> siz; // 素集合のサイズを表す配列(1 で初期化)
+    vector <ll> edge;
 
     // Constructor
-    UnionFind(ll sz_): par(sz_), siz(sz_, 1LL) {
+    UnionFind(ll sz_): par(sz_), siz(sz_, 1LL), edge(sz_) {
         for (ll i = 0; i < sz_; ++i) par[i] = i; // 初期では親は自分自身
     }
     void init(ll sz_) {
@@ -25,10 +26,14 @@ struct UnionFind {
     bool merge(ll x, ll y) {
         x = root(x);
         y = root(y);
-        if (x == y) return false;
+        if (x == y) {
+            edge[x]++;
+            return false;
+        }
         // merge technique（データ構造をマージするテク．小を大にくっつける）
         if (siz[x] < siz[y]) swap(x, y);
         siz[x] += siz[y];
+        edge[x] += edge[y] + 1;
         par[y] = x;
         return true;
     }
@@ -39,5 +44,9 @@ struct UnionFind {
 
     ll size(ll x) { // 素集合のサイズ
         return siz[root(x)];
+    }
+
+    ll cnt_edge(ll x) {
+        return edge[root(x)];
     }
 };
